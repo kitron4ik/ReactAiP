2064f017506b7f9f1ba51ff15227447cf4aa6811
kitron4ik <craftukww@gmail.com>
2024-02-03 12:52:28 +0300

    Ex22-Ex24

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -148,4 +148,70 @@ import React from 'react';
   </div>;
 }*/
 
+/*function App() {
+  return <div>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+    </ul>
+  </div>;
+}*/
+
+/*function App() {
+  return <table>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>4</td>
+        <td>6</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>16</td>
+        <td>36</td>
+      </tr>
+    </tbody>
+  </table>;
+}*/
+
+
+/*function App() { //В HTML коде покажется result: 9
+  const num1 = 3;
+  const num2 = 2;
+
+  return <div>
+    result: {num1 ** num2}
+  </div>;
+}*/
+
+/*function App() {  //result: john smit
+  const name = 'john';
+  const surname = 'smit';
+
+  return <div>
+    result: {name + ' ' + surname}
+  </div>;
+}*/
+
+function App() { //Покажется корень из 4-х, тоесть 2
+  const num = 4;
+
+  return <div>
+    result: {Math.sqrt(num)}
+  </div>;
+}
+
 export default App;
