4ddf46623817801cc95be21f9b5e398b8c0729af
kitron4ik <craftukww@gmail.com>
2024-02-03 12:37:57 +0300

    Ex1-Ex22

---
+++ my-app/src/App.js
@@ -0,0 +1,151 @@
+import React from 'react';
+
+/*function App() { //Упражнение 1-9
+  return <div>
+    Видоизмененный вид дива
+    <p class="p1">
+      Абзац 1
+    </p>
+    <p class="p2">
+      Абзац 2
+    </p>
+    <ul>
+      <li>
+        1
+      </li>
+      <li>
+        2
+      </li>
+      <li>
+        3
+      </li>
+      <li>
+        4
+      </li>
+      <li>
+        5
+      </li>
+      <li>
+        6
+      </li>
+      <li>
+        7
+      </li>
+      <li>
+        8
+      </li>
+      <li>
+        9
+      </li>
+      <li>
+        10
+      </li>
+    </ul>;
+  </div>;
+}*/
+/*function App() { //return нужно писать на одной строчке с тегом, который мы хотим вернуть
+  return <ul>
+    <li>text1</li>
+    <li>text2</li>
+    <li>text3</li>
+  </ul>;
+}*/
+/*function App() {
+  return <div>
+    <ul>
+      <li>text1</li>
+      <li>text2</li>
+      <li>text3</li>
+    </ul>
+    <ul>
+      <li>text1</li>
+      <li>text2</li>
+      <li>text3</li>
+    </ul>;
+    <input />;
+  </div>
+}*/
+
+/*function App() {
+  return <input />;
+}*/
+
+/*function App() {
+  const str1 = 'text1';
+  const str2 = 'text1';
+
+  return <div>
+    <p>{str1}</p>
+    <p>{str2}</p>
+  </div>;
+}*/
+
+/*function App() {
+  const name = 'user';
+  const age = '30';
+
+  return <div>
+    name: {name}
+    age:  {age}
+  </div>;
+}*/
+
+/*function App() {
+  const arr = [1, 2, 3, 4, 5];
+  return <div>
+    <ul>
+      <li>{arr[0]}</li>
+      <li>{arr[1]}</li>
+      <li>{arr[2]}</li>
+      <li>{arr[3]}</li>
+      <li>{arr[4]}</li>
+    </ul>
+  </div>
+}*/
+
+/*function App() {
+  const obj = { name: 'john', surname: 'smit' };
+  return <div>
+    <p>{obj.name}</p>
+    <p>{obj.surname}</p>
+  </div>
+}*/
+
+/*function App() {
+  const attr = 'block';
+
+  return <div class={attr}>
+    Блок
+  </div>;
+}*/
+
+/*function App() {
+  const li1 = <li>text1</li>;
+  const li2 = <li>text2</li>;
+  const li3 = <li>text3</li>;
+
+  return <ul>
+    <li>{li1}</li>
+    <li>{li2}</li>
+    <li>{li3}</li>
+  </ul>
+}*/
+
+/*function App() {
+  const items = <ul><li>text1</li><li>text2</li><li>text3</li></ul>;
+
+  return <ul>
+    {items}
+  </ul>
+}*/
+
+/*function App() {
+  return <div>
+    1<input /><br />
+    2<input /><br />
+    3<input /><br />
+    4<input /><br />
+  </div>;
+}*/
+
+export default App;
